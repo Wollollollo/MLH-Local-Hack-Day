339c5667fef0d1ee0f7b468cfb82f0fefc745c55
HalmonLui <halmonlui@gmail.com>
2017-12-02 12:39:19 -0500

    Works
    Sends testing to phone

---
+++ send_sms.py
@@ -0,0 +1,13 @@
+import os
+from twilio.rest import Client
+
+account_sid = "AC024a6c69dc9d367255de079c80288594"
+auth_token = "408b734fbfd9b480f8d4392129718687"
+
+client = Client(account_sid, auth_token)
+
+client.messages.create(
+    to="6177330634",
+    from_="+16178299666",
+    body="testing lalala"
+)
